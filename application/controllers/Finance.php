74ebff644a8fc6753a6961d8a7208608a35d91bc
varsha <varsharamani232@gmail.com>
2019-09-27 17:24:18 +0530

    create invoice and insert data

--- application/controllers/Finance.php
+++ application/controllers/Finance.php
@@ -180,7 +180,7 @@ class Finance extends CI_Controller
 				                <button type="button" class="btn btn-outline-info dropdown-toggle" data-toggle="dropdown">Action  <span class="caret"></span></button>
 				                		<div class="dropdown-menu">
 						                    <a  class="dropdown-item" href='.base_url().'Finance/editestimate/'.base64_encode($id).'><i class="fa fa-pencil"></i> Edit</a>
-											<a  class="dropdown-item" href="javascript:void()" onclick="deleteestimates(\''.base64_encode($row->id).'\')"><i class="fa fa-trash "></i> Delete</a>
+											<a  class="dropdown-item" href="javascript:void(0)" onclick="deleteestimates(\''.base64_encode($row->id).'\')"><i class="fa fa-trash "></i> Delete</a>
 											<a  class="dropdown-item" href='.base_url().'Finance/createinvoice/'.base64_encode($id).'><i class="ti-receipt"></i>Create Invoice</a>
 				               			 </div>
 							</div>';
@@ -271,44 +271,227 @@ class Finance extends CI_Controller
 		$data['estimate']=$this->common_model->getData('tbl_estimates',$whereArr);
 		$data['product']=$this->common_model->getData('tbl_products',$whereArr1);
 		$sql='SELECT * FROM tbl_invoice ORDER BY tbl_invoice.id DESC LIMIT 1';
+		//$sql="SELECT tbl_project_info.clientid,tbl_clients.clientname,tbl_clients.companyname FROM tbl_project_info INNER JOIN tbl_clients ON tbl_project_info.clientid = tbl_clients.id where tbl_project_info.id=47";			
+
 		$data['invoice']=$this->common_model->coreQueryObject($sql);
+		//print_r($data['invoice']);die;
 		$this->load->view('common/header');
 		$this->load->view('Estimates/createinvoice',$data);
 		$this->load->view('common/footer');
 
 		if($this->input->post('btnsubmit'))
 		{
-			$invoice=$this->input->post('txtinvoice');
-			$project=$this->input->post('txtproject');
-			$currency=$this->input->post('txtcurrency');
-			$invoicedate=$this->input->post('txtinvoicedate');
-			$duedate=$this->input->post('txtduedate');
-			$status=$this->input->post('txtstatus');
-			$recuringpayment=$this->input->post('txtrp');
-			$billingfrequency=$this->input->post('txtbf');
-			$billinginterval=$this->input->post('txtbi');
-			$billingcycle=$this->input->post('txtbc');
+			$invoice=$this->input->post('invoice_number');
+			$project=$this->input->post('project');
+			$currency=$this->input->post('currency');
+			$invoicedate=$this->input->post('invoice_date');
+			$duedate=$this->input->post('due_date');
+			$status=$this->input->post('status');
+			$recuringpayment=$this->input->post('recurring_payment');
+			$billingfrequency=$this->input->post('billing_frequency');
+			$billinginterval=$this->input->post('billing_interval');
+			$billingcycle=$this->input->post('billing_cycle');
 			$total=$this->input->post('txttotal');
-			$insertArr=array('invoice' => $invoice,'project' => $project,'currency' => $currency,'invoicedate' => $invoicedate,'duedate'=>$duedate,'status'=>$status,'recuringpayment'=>$recuringpayment,'billingfrequency'=>$billingfrequency,'billinginterval'=>$billinginterval,'billingcycle'=>$billingcycle,'total'=>$total);
+			//$sql=select clientid form tbl_project_info 
+			$sql="SELECT tbl_project_info.clientid,tbl_clients.clientname,tbl_clients.companyname FROM tbl_project_info INNER JOIN tbl_clients ON tbl_project_info.clientid = tbl_clients.id where tbl_project_info.id=".$project;			
+			$data['invoicedata']=$this->common_model->coreQueryObject($sql);
+			//echo "<pre>";print_r($data);die;
+
+			
+			$insertArr=array('invoice' => $invoice,'project' => $project,'clientname'=>$invoicedata[0]->clientid,'companyname'=>$invoicedata[0]->companyname,'currency' => $currency,'invoicedate' => $invoicedate,'duedate'=>$duedate,'status'=>$status,'recuringpayment'=>$recuringpayment,'billingfrequency'=>$billingfrequency,'billinginterval'=>$billinginterval,'billingcycle'=>$billingcycle,'total'=>$total);
+			print_r($insertArr);die;
 			$this->common_model->insertData('tbl_invoice',$insertArr);
 			$invoiceid=$this->db->insert_id();
 			
-			$item=$this->input->post('txtitem');
-			$qtyhrs=$this->input->post('txtqtyhrs');
-			$unitprice=$this->input->post('txtunitprice');
-			$tax=$this->input->post('txttax');
-			$amount=$this->input->post('txtamount');
-			$description=$this->input->post('txtdescription');
-			$count=count($this->input->post('txtitem'));
+			$item=$this->input->post('item_name');
+			$qtyhrs=$this->input->post('quantity');
+			$unitprice=$this->input->post('cost_per_item');
+			$tax=$this->input->post('tax');
+			$amount=$this->input->post('amount');
+			$description=$this->input->post('item_Description');
+			$count=count($this->input->post('item_name'));
 			for($i=0;$i<$count;$i++)
 			{
 				$insertArr1=array('invoiceid'=>$invoiceid,'item' => $item[$i],'qtyhrs' => $qtyhrs[$i], 'unitprice' => $unitprice[$i], 'tax' => $tax[$i],'amount'=>$amount[$i],'description' => $description[$i]);
 				$this->common_model->insertData('tbl_invoiceproduct',$insertArr1);
 			}
 				$this->session->set_flashdata('messagename', "Data Inserted Succeess");
-				redirect('Estimates/viewinvoicepage');
+				redirect('Finance/invoice');
+
 
 			}
 	}
+	
+	public function invoice(){
+		$this->load->view('common/header');
+		$this->load->view('Invoices/invoice');
+		$this->load->view('common/footer');
+	}
+	
+	public function invoice_list(){
+	//echo "cbjkdjkcd";die;
+	if(!empty($_POST)){
+			$_GET = $_POST;
+			$defaultOrderClause = "";
+			$sWhere = "";
+			$sOrder = '';	
+			$aColumns = array( 'id', 'invoice', 'project', 'clientname','total', 'invoicedate','status');
+			//'ahrefs_dr', 
+            $totalColumns = count($aColumns);
+
+			/** Paging Start **/
+            $sLimit = "";
+            $sOffset = "";
+            if ($_GET['iDisplayStart'] < 0) {
+                $_GET['iDisplayStart'] = 0;
+            }
+            if ($_GET['iDisplayLength'] < 0) {
+                $_GET['iDisplayLength'] = 10;
+            }
+            if (isset($_GET['iDisplayStart']) && $_GET['iDisplayLength'] != '-1') {
+                $sLimit = (int) substr($_GET['iDisplayLength'], 0, 6);
+                $sOffset = (int) $_GET['iDisplayStart'];
+            } else {
+                $sLimit = 10;
+                $sOffset = (int) $_GET['iDisplayStart'];
+            }
+            /** Paging End **/
+            /** Ordering Start **/
+            $noOrderColumns = array('other_do_ext');
+            if (isset($_GET['iSortCol_0']) && !in_array($aColumns[intval($_GET['iSortCol_0'])], $noOrderColumns)) {
+                $sOrder = " ";
+                for ($i = 0; $i < intval($_GET['iSortingCols']); $i++) {
+                    if ($_GET['bSortable_' . intval($_GET['iSortCol_' . $i])] == "true") {
+
+                        if ($aColumns[intval($_GET['iSortCol_' . $i])] != '') {
+                            $sOrder .= $aColumns[intval($_GET['iSortCol_' . $i])] . " " . $_GET['sSortDir_' . $i] . ", ";
+                        } 
+                        else {
+                            $sOrder = $defaultOrderClause . " ";
+                        }
+
+                        $sortColumnName = intval($_GET['iSortCol_' . $i]).'|'.$_GET['sSortDir_' . $i];
+                    }
+                }
+
+                $sOrder = substr_replace($sOrder, "", -2);
+                if ($sOrder == "ORDER BY") {
+                    $sOrder = "";
+                }
+            } else {
+                $sOrder = $defaultOrderClause;
+            }
+
+            if(!empty($sOrder)){
+            	$sOrder = " ORDER BY ".$sOrder;
+            }
+            /** Ordering End **/
+
+            /** Filtering Start */
+            if(!empty(trim($_GET['sSearch']))){
+            	$searchTerm = trim($_GET['sSearch']);
+            	$sWhere .= ' AND (project like "%'.$searchTerm.'%" OR clientname like "%'.$searchTerm.'%" OR companyname like "%'.$searchTerm.'%" OR note like "%'.$searchTerm.'%")';
+            }
+				$startdate=!empty($_POST['startdate']) ? $_POST['startdate'] : '';
+				$enddate=!empty($_POST['enddate']) ? $_POST['enddate'] : '';
+				$projectname=!empty($_POST['projectname']) ? $_POST['projectname'] : '';
+				$clientname=!empty($_POST['clientname']) ? $_POST['clientname'] : '';
+				$status=$_POST['status'];
+		
+				if(!empty($projectname)){
+							$sWhere.=' AND  project="'.$projectname.'"';
+
+					}
+					if(!empty($clientname)){
+							$sWhere.=' AND  clientname="'.$clientname.'"';
+
+					}
+					
+					if($status=='all'){
+						}else{
+								$sWhere.=' AND status='.$status;
+						}
+					if(!empty($startdate)){						
+						$sWhere.=' AND duedate>="'.$startdate.'"';
+					}
+					if(!empty($enddate)){						
+						$sWhere.=' AND duedate<="'.$enddate.'"';
+					}
+					
+					if(!empty($sWhere)){
+						$sWhere = " WHERE 1 ".$sWhere;
+					}
+					/** Filtering End */
+				}
+				
+		
+	    $query = "SELECT * from tbl_invoice ".$sWhere.' '.$sOrder.' limit '.$sOffset.', '.$sLimit;
+		$invoicesArr = $this->common_model->coreQueryObject($query);
+
+		$query = "SELECT * from tbl_invoice ".$sWhere;
+		//echo $query;die;
+		$invoicesFilterArr = $this->common_model->coreQueryObject($query);
+		$iFilteredTotal = count($invoicesFilterArr);
+
+		$invoicesAllArr = $this->common_model->getData('tbl_invoice');
+		$iTotal = count($invoicesAllArr);
+
+		/** Output */
+		$datarow = array();
+		$i = 1;
+		foreach($invoicesArr as $row) {
+			$id = $row->id;
+			if($row->status == '0'){
+				$status = $row->status = 'Unpaid';
+				$sta='<lable class="label label-danger">'.$status.'</label>';
+
+			}
+			else if($row->status == '1'){
+				$status = $row->status = 'Paid';
+				$sta='<lable class="label label-success">'.$status.'</label>';
+			}
+			else if($row->status == '2'){
+				$status = $row->status = 'Partially Paid';
+				$sta='<lable class="label label-info">'.$status.'</label>';
+			}
+			//$clientid = $row->clientid;
+			//$create_date = date('d-m-Y', strtotime($row->created_at));
+			
+				$actionStr = '<div class="dropdown action m-r-10">
+				                <button type="button" class="btn btn-outline-info dropdown-toggle" data-toggle="dropdown">Action  <span class="caret"></span></button>
+				                		<div class="dropdown-menu">
+						                    <a  class="dropdown-item" href='.base_url().'Finance/editestimate/'.base64_encode($id).'><i class="fa fa-pencil"></i> Edit</a>
+											<a  class="dropdown-item" href="javascript:void()" onclick="deleteestimates(\''.base64_encode($row->id).'\')"><i class="fa fa-trash "></i> Delete</a>
+											<a  class="dropdown-item" href='.base_url().'Finance/createinvoice/'.base64_encode($id).'><i class="ti-receipt"></i>Create Invoice</a>
+				               			 </div>
+							</div>';
+			
+			
+			$datarow[] = array(
+				$id = $i,
+                $row->invoice,
+                $row->project,
+                $row->clientname,
+                $row->total,
+                $row->project,
+				$row->invoicedate,
+                $sta,	
+				$actionStr
+           	);
+           	$i++;
+      	}
+        
+		$output = array
+		(
+		   	"sEcho" => intval($_GET['sEcho']),
+	        "iTotalRecords" => $iTotal,
+	        "iTotalRecordsFormatted" => number_format($iTotal), //ShowLargeNumber($iTotal),
+	        "iTotalDisplayRecords" => $iFilteredTotal,
+	        "aaData" => $datarow
+		);
+	  echo json_encode($output);
+      exit();
+	}
+	
 }
 ?>
--- assets/js/varsha.js
+++ assets/js/varsha.js
@@ -316,3 +316,92 @@ function deleteestimates(estimateid){
 	   }
 	   });
 }
+
+//show &hide
+ $("select").change(function(){
+  if ( this.value == '1')
+  {
+	$(".showdiv").show();
+  }
+  else
+  {
+    $(".showdiv").hide();
+  }
+});
+
+//invoicelist
+
+jQuery(document).ready(function() {
+	if(controllerName == 'finance' && (functionName == 'index' || functionName == '')){
+		var oTable = jQuery('#invoices').DataTable({
+			'bRetrieve': true,
+	        "bPaginate": true,
+	        "bLengthChange": true,
+	        "iDisplayLength": 10,
+	        "bFilter": true,
+	        "bSort": true,
+	        "aaSorting": [],
+	        "aLengthMenu": [[10, 25, 50, 100, 200, 500, 1000, 5000], [10, 25, 50, 100, 200, 500, 1000, 5000]],
+	        "bInfo": true,
+	        "bAutoWidth": false,
+	        "bProcessing": true,
+	        "aoColumns": [{ "sWidth": "40px", sClass: "text-left", "asSorting": [  ] }, 
+                      { "sWidth": "250px", sClass: "text-center", "asSorting": [ "desc", "asc" ] }, 
+                      { "sWidth": "250px", sClass: "text-center", "asSorting": [ "desc", "asc" ] }, 
+                      { "sWidth": "250px", sClass: "text-center", "asSorting": [ "desc", "asc" ] }, 
+                      { "sWidth": "250px", sClass: "text-center", "asSorting": [  ]}, 
+                      { "sWidth": "250px", sClass: "text-center", "asSorting": [ "desc", "asc" ] }, 
+                      { "sWidth": "250px", sClass: "text-center", "asSorting": [  ]}, 
+					  { "sWidth": "250px", sClass: "text-center", "asSorting": [  ]}, 
+
+                     ],
+	        "bServerSide": true,
+	        "fixedHeader": true,
+	        "sAjaxSource": base_url+"Finance/invoice_list",
+	        "sServerMethod": "POST",
+	        "sDom": "<'row'<'col-sm-6'l><'col-sm-6'f>>t<'row'<'col-sm-6'i><'col-sm-6'p>>",
+        	"oLanguage": { "sProcessing": "<i class='fa fa-spinner fa-spin fa-3x fa-fw green bigger-400'></i>", "sEmptyTable": '<center><br/>No Leads found<br/><br/></center>', "sZeroRecords": "<center><br/>No Leads found<br/><br/></center>", "sInfo": "_START_ to _END_ of _TOTAL_ leads", "sInfoFiltered": "", "oPaginate": {"sPrevious": "<i class='fa fa-angle-double-left'></i>", "sNext": "<i class='fa fa-angle-double-right'></i>"}},
+        	"fnServerData": function ( sSource, aoData, fnCallback, oSettings ) {
+			
+					aoData.push( { "name": "startdate", "value": $('#startdate').val() } );
+					aoData.push( { "name": "enddate", "value": $('#enddate').val() } );
+					aoData.push( { "name": "projectname", "value": $('#projectname').val() } );
+					aoData.push( { "name": "clientname", "value": $('#clientname').val() } );
+					aoData.push( { "name": "status", "value": $('#status').val() } );
+					
+			
+        		oSettings.jqXHR = $.ajax( {
+	                "dataType": 'json',
+	                "type": "POST",
+	                "url": sSource,
+	                "data": aoData,
+	                "timeout": 60000, //1000 - 1 sec - wait one minute before erroring out = 30000
+	                "success": function(json) {
+	                    var oTable = $('#invoices').dataTable();
+	                    var oLanguage = oTable.fnSettings().oLanguage;
+
+	                    if((json.estimateCount == true) && (json.iTotalDisplayRecords == json.limitCountQuery)){
+	                        oLanguage.sInfo = '<b>_START_ to _END_</b> of more than _TOTAL_ (<small>' + json.iTotalRecordsFormatted + ' Clients</small>)';
+	                    }
+	                    else{
+	                        oLanguage.sInfo = '<b>_START_ to _END_</b> of <b>_TOTAL_</b> (<small>' + json.iTotalRecordsFormatted + ' Clients</small>)';
+	                    }
+	                    
+	                    fnCallback(json);
+	                }
+	            });
+        	},
+        	"fnRowCallback": function( nRow, aData, iDisplayIndex ){
+                return nRow;
+	        },
+	        "fnDrawCallback": function(oSettings, json) {
+
+	        },
+		});
+	}
+});
+$('#btnapply').click(function(){ //button filter event click
+				var oTable = $('#invoices').DataTable();
+					oTable.draw();
+			});
+				
