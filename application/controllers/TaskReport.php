bec0db6cc79c92b29890af1cb1d8ae8d784b29a9
vaishali <vaishalitank165@gmail.com>
2020-05-17 11:37:18 +0530

    task report list

--- application/controllers/TaskReport.php
+++ application/controllers/TaskReport.php
@@ -151,7 +151,7 @@ class TaskReport extends CI_Controller {
 
 			if($row->status=='0'){
 					$status=$row->status='Incomplete';
-					$showStatus = '<label class="label label-success">'.$status.'</label>';
+					$showStatus = '<label class="label label-danger">'.$status.'</label>';
 			}
 			else if($row->status=='1'){
 					$status=$row->status='Todo';
@@ -164,7 +164,7 @@ class TaskReport extends CI_Controller {
 			
 			else if($row->status=='3'){
 					$status=$row->status='Done';
-					$showStatus = '<label class="label label-danger">'.$status.'</label>';
+					$showStatus = '<label class="label label-success">'.$status.'</label>';
 			}
 			
 			else if($row->status=='4'){
