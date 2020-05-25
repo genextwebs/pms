be55bbd0c2417c747f52fd32d5fa344551e387ce
varsha <varsharamani232@gmail.com>
2020-05-25 18:06:23 +0530

    edit

--- application/views/common/clientsidebar.php
+++ application/views/common/clientsidebar.php
@@ -83,7 +83,7 @@ if($this->session->userdata('login')){
             </a>
         </li>
          <li>
-            <a href="<?php echo base_url().'noticeboard'?>" class="nav-link-s">
+            <a href="<?php echo base_url().'Noticeboard'?>" class="nav-link-s">
                 <i class="ti-layout-media-overlay"></i>
                 <span>Notice Board</span>
             </a>
--- application/views/common/sidebar.php
+++ application/views/common/sidebar.php
@@ -162,8 +162,8 @@ if($this->session->userdata('login')){
                 <span>Leaves</span>
             </a>
         </li>
-        <li <?php if($controller == 'noticeboard' && ($functionName == 'index' || $functionName == '')) { echo 'class="active"'; } ?>>
-            <a href="<?php echo base_url().'noticeboard'?>" class="nav-link-s">
+        <li <?php if($controller == 'Noticeboard' && ($functionName == 'index' || $functionName == '')) { echo 'class="active"'; } ?>>
+            <a href="<?php echo base_url().'Noticeboard'?>" class="nav-link-s">
                 <i class="ti-layout-media-overlay"></i>
                 <span>Notice Board</span>
             </a>
